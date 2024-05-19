f7d4e3dade02240332655b91e6a5f9a4cd042519
Chethiya Hitihamu <115537042+ChethiyaNuwan@users.noreply.github.com>
2024-05-19 17:12:57 +0530

    Refactor component structure and fix styling issues

--- src/Components/SwitchedBody.tsx
+++ src/Components/SwitchedBody.tsx
@@ -1,14 +1,27 @@
 import Bodyy from "./Body";
 import BodyTwo from "./BodyTwo";
+import { CartProvider } from "./CartContext";
+import ShoppingCart from "./ShopingCart";
 
 interface SwitchedBodyProps {
     index: number;
 }
 
 export default function SwitchedBody({ index }: SwitchedBodyProps) {
+    let body = null;
+
     switch(index){
-        case 0: return <BodyTwo/>
-        case 1: return <BodyTwo/>
-        case 2: return <Bodyy/>
+        case 0: body = <BodyTwo/>; break;
+        case 1: body = <BodyTwo/>; break;
+        case 2: body = <Bodyy/>; break;
     }
+
+    return (
+        <CartProvider>
+            <div style={{display:'flex', background:'blanchedalmond'}}>
+                {body}
+                <ShoppingCart />
+            </div>
+        </CartProvider>
+    )
 }
